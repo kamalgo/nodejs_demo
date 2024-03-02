f456e9c1aae1dbe8ee6cd7535672b30bc6a8b2ad
kamalgo <kamal.252534@gmail.com>
2024-03-02 18:48:24 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var a = 6;
-var b=3;
-console.log(a+b);
+// Calculator Program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+}
